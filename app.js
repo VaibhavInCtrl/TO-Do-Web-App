304cd651f158b54c5c19ce057c5fdb388f508353
Vaibhav Agarwal <vaibhav_iitd24@Vaibhavs-MacBook-Air.local>
2021-05-19 12:43:54 +0530

    Added work list

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
-const { create } = require("domain");
 const express = require("express");
-const https = require("https");
+
 const app = express();
-app.use(express.urlencoded({extended:true}))
-app.listen( process.env.PORT || "3000",(req,res)=>{
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(process.env.PORT || "3000", (req, res) => {
     console.log("Server up and running on port 3000");
 })
-app.set("view engine","ejs");
-var inputs = ["What do you want to do today?"];
-app.post("/",(req,res)=>{
-    var input = req.body.create;
-    inputs.push(input);
-    res.redirect("/")
-})
 
-app.get("/",(req,res)=>{
+app.set("view engine", "ejs");
+
+app.use(express.static('Public'));
+
+var inputs = ["What do you want to do today?"];
+
+var worklist = ["What work do you have today?"]
+app.get("/", (req, res) => {
     var date = new Date();
     var printer = "";
     var options = {
         weekday: "long",
-        year: "numeric",
-        month: "long"
-    } 
-    printer = (date.toLocaleDateString('en', options));
-    console.log(printer);
-    res.render("list",{
-        printer:printer,
-        inputs:inputs
+        month: "long",
+        day: "numeric"
     }
-    )
+    printer = (date.toLocaleDateString('en', options));
+
+    res.render("list", {
+        printer: printer,
+        inputs: inputs
+    })
 })
-app.use(express.static('Public'))
+
+app.post("/", (req, res) => {
+    var input = req.body.create;
+    if (req.body.list === "Work"){
+        worklist.push(input);
+        res.redirect("/work");
+    }
+    else{
+        inputs.push(input);
+        res.redirect("/")
+    }
+    
+})
+
+
+app.get("/work", (req, res) => {
+    res.render("list", {
+        printer: "Work",
+        inputs: worklist
+    })
+})
